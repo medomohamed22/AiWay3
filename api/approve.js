f46c90f6ade96dac2efeab649cb163d5c5ea2828
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2026-07-12 17:51:07 +0300

    Delete api directory

--- api/approve.js
+++
@@ -1,6 +0,0 @@
-import paymentHandler from './pi-payment.js';
-
-export default function handler(req, res) {
-  req.body = { ...(req.body || {}), action: 'approve' };
-  return paymentHandler(req, res);
-}
--- api/complete.js
+++
@@ -1,6 +0,0 @@
-import paymentHandler from './pi-payment.js';
-
-export default function handler(req, res) {
-  req.body = { ...(req.body || {}), action: 'complete' };
-  return paymentHandler(req, res);
-}
